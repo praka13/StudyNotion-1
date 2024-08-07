2aabe1716cfe5ddd53fa22eeae71ea8cd4b5a1c3
Aditya Prakash <140341906+praka13@users.noreply.github.com>
2024-08-07 09:28:23 +0530

    Update Template.js

--- src/components/common/Template.js
+++ src/components/common/Template.js
@@ -24,10 +24,7 @@ const Template = ({title,desc1,desc2,image,formtype}) => {
                 <div className='w-full h-[1px] bg-richblack-700'></div>
             </div>
            
-            <button className='w-full flex justify-center items-center rounded-[8px] font-medium text-richblack-100 border border-richblack-700 px-[12px] py-[8px] gap-x-2 mt-6'>
-                <FcGoogle></FcGoogle>
-                <p>Sign up with Google</p>
-            </button>
+
         </div>
 
         <div className='relative w-11/12 max-w-[450px]'>
